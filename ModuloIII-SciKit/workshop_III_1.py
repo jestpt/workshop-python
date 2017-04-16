2e0d5127ef3fee3de8dc306b149d0041867e3fc4
gaborod16 <gaborod16@gmail.com>
2017-04-16 15:54:03 +0100

    Merge Module III part I

--- ModuloIII-SciKit/workshop_III_1.py
+++ ModuloIII-SciKit/workshop_III_1.py
@@ -13,6 +13,7 @@
 import sklearn as skl
 import pandas as pd
 import seaborn as sns; sns.set(style="ticks", color_codes=True)
+import os
 
 #secondly, we import the sample datasets from skl to work
 from sklearn import datasets
@@ -21,16 +22,36 @@ from sklearn import datasets
 iris = datasets.load_iris()
 
 #let's look at our dataset
+print('# --- How is our data stored?')
+print(type(iris))
 print(iris)
+os.system('pause')
+os.system('cls')
+print('# --- What are the keys of this dictionary?')
+print(iris.keys())
+os.system('pause')
+print('# --- What is the data?')
+print(iris['feature_names'])
+print(iris['data'])
+print('Ammount of data: ' + str(len(iris['data'])))
+os.system('pause')
+print('# --- What is the target?')
+print(iris['target_names'])
+print(iris['target'])
+print('Ammount of targets: ' + str(len(iris['data'])))
+os.system('pause')
+os.system('cls')
 
 #so let's separate our data (X) from our target(y)
-X = iris.data
-y = iris.target
+X = iris['data']
+y = iris['target']
 
 #now we'll use pandas to name the columns and have a nice look at our features
-iris_table = pd.DataFrame(X, columns = iris.feature_names)
-
+iris_table = pd.DataFrame(X, columns = iris['feature_names'])
+print('# --- Better visualization with Pandas')
 print(iris_table.head())
+os.system('pause')
+os.system('cls')
 
 #to do some cool visual EDA we'll use seaborn that we already imported as sns
 
@@ -42,13 +63,19 @@ sns.plt.show()
 from sklearn.neighbors import KNeighborsClassifier
 
 #after import we create the classifier as we like
-knn = KNeighborsClassifier(n_neighbors = 6)
+#to avoid draws, it is recommended that the number of neighbors selected is not multiple of:
+#		x, being x in the set of {2, ... n_classes}
+#		In this case we have three classes, so n_neighbors should not be multiple of 2 or 3 (n_classes = 3)
+#		Therefore, n_neighbors could be: 5, 7, 11, ...
+knn = KNeighborsClassifier(n_neighbors = 5)
 
 #using X and y defined previously we'll fit our dataset to the classifier we just created
 knn.fit(X, y)
 
 #now, although it's not correct, let's test our model against the dataset we used to create it
 print(knn.score(X, y))
+os.system('pause')
+os.system('cls')
 
 #########################################################################
 ############### II - TRAIN / TEST SPLIT + FIT / ACCURACY ################
@@ -80,12 +107,12 @@ print(knn.score(X_test, y_test))
 #now, we are going to use predict() to see how we can use our classifier on new, unidentified samples
 #for example, we have new data X
 
-X_new = [ 5.7,  3.8,  1.7,  0.3]
+X_new = [5.7, 3.8, 1.7, 0.3]
 
 #we already trained and fitted our classifier so let's put it to use
 
 new_prediction = knn.predict(X_new)
 
-print(new_prediction)
+print('Classified as: ' + str(iris['target_names'][new_prediction]))
 
 #So, with an accuracy of 0.966666666667 we can say it's a Iris Setosa
